d5898426ee4ad6cb2a40e1ed6b8d12d949314588
szincone <sawyerzincone@gmail.com>
2018-08-24 23:23:24 -0600

    added css reset to index.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import App from "./App";
 
 import "font-awesome/css/font-awesome.min.css";
 import "./css/index.css";
+import "./css/reset.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
