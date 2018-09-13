5a0e8aab852be5a1e878b465ed24c26c95e0203f
szincone <sawyerzincone@gmail.com>
2018-09-12 20:27:13 -0600

    added styles

--- src/components/aboutpage/AboutPage.js
+++ src/components/aboutpage/AboutPage.js
@@ -9,15 +9,21 @@ export const AboutPage = () => {
       <h1>About Me</h1>
       <h4>
         My name is Sawyer Zincone and I am a full-stack developer with a passion
-        for meaningful UI design, writing clean & efficient code, and
-        collaboration. I enjoy tackling complex problems and turning them into
-        simple, easy-to-understand designs.
+        for meaningful <span style={{ color: "#EE4266" }}>UI design, </span>
+        writing clean &{" "}
+        <span style={{ color: "#ECC30B" }}>efficient code,</span> and
+        <span style={{ color: "#ECC30B" }}> collaboration</span>. I'm mainly
+        working in <span style={{ color: "#ECC30B" }}> Javascript</span> or{" "}
+        <span style={{ color: "#EE4266" }}> Python</span> and enjoy tackling
+        complex problems while turning them into simple, easy-to-understand
+        designs.
       </h4>
       <i className="fa fa-dribbble" aria-hidden="true" />
       <h4>
         I am a former math teacher who has a deep love for making things that
-        make a difference. When I'm not coding I enjoy playing guitar, working
-        out, or relaxing with my wife and cats.
+        make a difference and help improve people's lives. When I'm not
+        programming I enjoy playing guitar, working out, or relaxing with my
+        wife and crazy cats.
       </h4>
       <Link to="/">
         <button className="button">
