d0d797d83aa8381f57f04998a72517b80fa5d4e9
szincone <sawyerzincone@gmail.com>
2019-03-13 18:17:08 -0600

    updated bio on about page

--- src/components/aboutpage/AboutPage.js
+++ src/components/aboutpage/AboutPage.js
@@ -1,29 +1,27 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import "../../css/aboutpage.css";
+import '../../css/aboutpage.css';
 
 export const AboutPage = () => {
   return (
     <div className="aboutPageDiv">
       <h1>About Me</h1>
       <h4>
-        My name is Sawyer Zincone and I am a full-stack developer with a passion
-        for meaningful <span style={{ color: "#EE4266" }}>UI design, </span>
-        writing clean &{" "}
-        <span style={{ color: "#ECC30B" }}>efficient code,</span> and
-        <span style={{ color: "#ECC30B" }}> collaboration</span>. I'm mainly
-        working in <span style={{ color: "#ECC30B" }}> Javascript</span> or{" "}
-        <span style={{ color: "#EE4266" }}> Python</span> and enjoy tackling
-        complex problems while turning them into simple, easy-to-understand
-        designs.
+        My name is Sawyer Zincone and I'm a Full-Stack Engineer with a passion
+        for writing<span style={{ color: '#EE4266' }}> clean </span>&{' '}
+        <span style={{ color: '#ECC30B' }}>efficient code,</span> and
+        <span style={{ color: '#ECC30B' }}> collaboration</span>. Lifelong
+        learner with an insatiable{' '}
+        <span style={{ color: '#EE4266' }}>curiosity</span> for new
+        technologies/languages and is able to{' '}
+        <span style={{ color: '#ECC30B' }}>implement</span> these ideas or break
+        them down into simple, easy-to-understand designs.
       </h4>
       <i className="fa fa-dribbble" aria-hidden="true" />
       <h4>
-        I am a former math teacher who has a deep love for making things that
-        make a difference and help improve people's lives. When I'm not
-        programming I enjoy playing guitar, working out, or relaxing with my
-        wife and cats.
+        When I'm not programming I'm usually playing guitar, working-out, or
+        relaxing with my wife and cats.
       </h4>
       <Link to="/">
         <button className="button">
