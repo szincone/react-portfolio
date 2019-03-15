7cef36b9d7d1e7b8a16539283b63af015e36f92a
szincone <sawyerzincone@gmail.com>
2019-03-14 23:22:01 -0600

    added eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,7 @@ module.exports = {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'implicit-arrow-linebreak': ['error', 'beside'],
     'react/jsx-one-expression-per-line': 'off',
+    'arrow-parens': ['error', 'always'],
+    'object-curly-newline': ['error', { multiline: true }],
   },
 };
