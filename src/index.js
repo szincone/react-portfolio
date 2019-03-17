1b352b03428560bf47eea124b2359e5604c94183
szincone <sawyerzincone@gmail.com>
2019-03-17 04:32:54 -0600

    added material :sparkles:

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'font-awesome/css/font-awesome.min.css';
-import './css/index.css';
-import './css/reset.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import materialTheme from './theme';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <MuiThemeProvider theme={materialTheme}>
+    <CssBaseline />
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById('root'),
+);
