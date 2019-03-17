ff935f7b2b8a7413f39d15c4fd8c51705b119510
szincone <sawyerzincone@gmail.com>
2019-03-16 20:42:48 -0600

    added default&propTypes (button links)

--- src/components/buttonlinks/ButtonLinks.js
+++ src/components/buttonlinks/ButtonLinks.js
@@ -5,32 +5,25 @@ import PropTypes from 'prop-types';
 // import '../../css/buttonlinks.css';
 
 const styles = (theme) => ({
-  aboutPageDiv: {
-    padding: '1rem',
-    paddingTop: '5rem',
-    zIndex: '1',
-    position: 'relative',
-  },
-  mainTitle: {
-    fontFamily: '"Rubik Mono One", sans-serif',
-    fontSize: '2.2rem',
-    marginBottom: '.8rem',
-  },
-  homeLinkButton: {
-    margin: '1.5rem 0.5rem',
+  button: {
+    margin: '0.5rem',
     padding: '0.5rem 2rem',
     fontSize: '2rem',
-    background: theme.palette.primary.accent,
+    background: '#d7263d',
     color: 'white',
     borderRadius: '35px',
     border: '2px solid white',
     zIndex: '1',
     cursor: 'pointer',
   },
-  linkDec: { textDecoration: 'none' },
+  aLink: {
+    zIndex: '1',
+    color: theme.palette.secondary,
+  },
 });
 
 function ButtonLinks(props) {
+  const { classes } = props;
   return (
     <Grid className="mainLinksDiv">
       <a
@@ -39,7 +32,7 @@ function ButtonLinks(props) {
         rel="noopener noreferrer"
         className="tooltip"
       >
-        <Button className="button">
+        <Button className={classes.button}>
           <i className="fa fa-github" aria-hidden="true" />
           <span className="tooltiptext">GitHub</span>
         </Button>
@@ -50,7 +43,7 @@ function ButtonLinks(props) {
         rel="noopener noreferrer"
         className="tooltip"
       >
-        <Button className="button">
+        <Button className={classes.button}>
           <i className="fa fa-linkedin" aria-hidden="true" />
           <span className="tooltiptext">LinkedIn</span>
         </Button>
@@ -61,13 +54,13 @@ function ButtonLinks(props) {
         rel="noopener noreferrer"
         className="tooltip"
       >
-        <Button className="button">
+        <Button className={classes.button}>
           <i className="fa fa-envelope" aria-hidden="true" />
           <span className="tooltiptext">Mail</span>
         </Button>
       </a>
       <Link to="/about" className="tooltip">
-        <Button className="button aboutMeButton">
+        <Button className={classes.button}>
           <i className="fa fa-user" aria-hidden="true" />
           <span className="tooltiptext">About</span>
         </Button>
@@ -75,5 +68,7 @@ function ButtonLinks(props) {
     </Grid>
   );
 }
+ButtonLinks.propTypes = { classes: PropTypes.objectOf(PropTypes.string) };
+ButtonLinks.defaultProps = { classes: styles };
 
 export default withStyles(styles)(ButtonLinks);
