4e3d7013487057d1677fa093b8e193d695de362e
szincone <sawyerzincone@gmail.com>
2019-03-17 04:27:32 -0600

    updated to match new paths

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Particle from './particles/Particle';
-import { HomePage } from './homepage/HomePage';
-import { AboutPage } from './aboutpage/AboutPage';
-import { ButtonLinks } from './buttonlinks/ButtonLinks';
+import HomePage from './homepage/HomePage';
+import AboutPage from './aboutpage/AboutPage';
+import ButtonLinks from './buttonlinks/ButtonLinks';
 
 export { AboutPage, ButtonLinks, HomePage, Particle };
