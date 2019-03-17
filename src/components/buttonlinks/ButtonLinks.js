0a0d4e5767445275a16c5369a7b8a7d8ec2494a4
szincone <sawyerzincone@gmail.com>
2019-03-17 00:06:56 -0600

    added prop-types and default props

--- src/components/buttonlinks/ButtonLinks.js
+++ src/components/buttonlinks/ButtonLinks.js
@@ -2,50 +2,41 @@ import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { Button, Grid, Link, withStyles } from '@material-ui/core';
 import PropTypes from 'prop-types';
-// import '../../css/buttonlinks.css';
 
 const styles = (theme) => ({
   button: {
     margin: '0.5rem',
     padding: '0.5rem 2rem',
     fontSize: '2rem',
-    background: '#d7263d',
-    color: 'white',
+    background: theme.palette.primary.accent,
+    color: theme.palette.secondary.main,
     borderRadius: '35px',
-    border: '2px solid white',
+    border: `2px solid ${theme.palette.secondary.main}`,
     zIndex: '1',
     cursor: 'pointer',
   },
 });
 
 function ButtonLinks(props) {
-  const { classes } = props;
+  const { classes, urls } = props;
   return (
     <Grid>
-      <Link underline="none" href="https://github.com/szincone">
+      <Link target="_blank" underline="none" href={urls.github}>
         <Button className={classes.button}>
           <i className="fa fa-github" aria-hidden="true" />
         </Button>
       </Link>
-      <Link
-        target="_blank"
-        underline="none"
-        href="https://www.linkedin.com/in/szincone/"
-      >
+      <Link target="_blank" underline="none" href={urls.linkedin}>
         <Button className={classes.button}>
           <i className="fa fa-linkedin" aria-hidden="true" />
         </Button>
       </Link>
-      <Link
-        target="_blank"
-        underline="none"
-        href="mailto:szincone.work@gmail.com"
-      >
+      <Link target="_blank" underline="none" href={urls.email}>
         <Button className={classes.button}>
           <i className="fa fa-envelope" aria-hidden="true" />
         </Button>
       </Link>
-      <Link component={RouterLink} to="/about">
+      <Link component={RouterLink} underline="none" to="/about">
         <Button className={classes.button}>
           <i className="fa fa-user" aria-hidden="true" />
         </Button>
@@ -53,7 +44,17 @@ function ButtonLinks(props) {
     </Grid>
   );
 }
-ButtonLinks.propTypes = { classes: PropTypes.objectOf(PropTypes.string) };
-ButtonLinks.defaultProps = { classes: styles };
+ButtonLinks.propTypes = {
+  classes: PropTypes.objectOf(PropTypes.string),
+  urls: PropTypes.objectOf(PropTypes.string),
+};
+ButtonLinks.defaultProps = {
+  classes: styles,
+  urls: {
+    github: 'https://github.com/szincone/',
+    linkedin: 'https://www.linkedin.com/in/szincone/',
+    email: 'mailto:szincone.work@gmail.com',
+  },
+};
 
 export default withStyles(styles)(ButtonLinks);
