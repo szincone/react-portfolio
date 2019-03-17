c8ebb0d3f14ee3afa9ed2389bffa6cca651f5a4a
szincone <sawyerzincone@gmail.com>
2019-03-17 04:24:22 -0600

    refactored styles and proptypes :recycle:

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Grid, withStyles } from '@material-ui/core';
+import PropTypes from 'prop-types';
 import { AboutPage, ButtonLinks, HomePage, Particle } from './components';
-import './css/app.css';
 
-export default function App() {
+const styles = (theme) => ({
+  appContainer: {
+    textAlign: 'center',
+    margin: '0 auto',
+    fontSize: '62.5%',
+    fontFamily: '"Oswald", sans-serif',
+    background: theme.palette.primary.main,
+    color: theme.palette.secondary.main,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    minHeight: '100vh',
+  },
+  containerWidth: { maxWidth: '800px' },
+});
+
+function App({ classes }) {
   const person = {
     name: 'Sawyer Zincone',
     skill: 'Full-Stack Engineer',
     email: 'szincone.work@gmail.com',
   };
+  const urls = {
+    github: 'https://github.com/szincone/',
+    linkedin: 'https://www.linkedin.com/in/szincone/',
+    email: 'mailto:szincone.work@gmail.com',
+  };
   return (
-    <div className="app">
-      <Router basename={process.env.PUBLIC_URL}>
-        <div className="appWidthDiv">
+    <Grid className={classes.appContainer}>
+      <Router>
+        <Grid className={classes.containerWidth}>
           <Route
             exact
             path="/"
@@ -21,12 +43,16 @@ export default function App() {
           <Route
             exact
             path="/"
-            render={(props) => <ButtonLinks {...props} />}
+            render={(props) => <ButtonLinks {...props} urls={urls} />}
           />
           <Route path="/about" component={AboutPage} />
-        </div>
+        </Grid>
       </Router>
       <Particle />
-    </div>
+    </Grid>
   );
 }
+App.propTypes = { classes: PropTypes.objectOf(PropTypes.string) };
+App.defaultProps = { classes: styles };
+
+export default withStyles(styles)(App);
